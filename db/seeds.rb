bdffcd0804ee388a46680347949df7d613c0ba60
frjumi <Frolova.Y.M@mail.ru>
2026-05-18 21:15:24 +0300

    lab_3 part 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(name: "Admin", email: "admin@example.com")
+User.create(name: "Test User", email: "test@example.com")
