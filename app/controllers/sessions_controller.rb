44a24ec738ebb042628db412597f18e6be7f4251
frjumi <Frolova.Y.M@mail.ru>
2026-05-19 00:33:12 +0300

    lab_7 part 2

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+    # форма входа
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to work_path   # после входа переходим в рабочую область
+    else
+      flash.now[:alert] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_url
+  end
+end
